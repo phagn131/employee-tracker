b1df9b632cb9771677c7d177a1e1947425edb843
phagn131 <>
2023-02-21 11:54:21 -0500

    running workbench queries to fix bug

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,26 +4,26 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id` INT NOT NULL,
   `deparment_name` VARCHAR(30) NOT NULL,
-  `department_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY
-);
+  `department_id` INT NOT NULL
+    );
 
-CREATE TABLE role (	
-  `role_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+CREATE TABLE roles (	
+  `role_id` INT NOT NULL,
 	`job_title` VARCHAR(30) NOT NULL,
   `salary` DECIMAL,
-  `department_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+  `department_id` INT NOT NULL
  
-);
+    );
 
 CREATE TABLE employee (
-  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id` INT NOT NULL,
   `first_name` VARCHAR(30) NOT NULL,
   `last_name` VARCHAR(30) NOT NULL,
-  `role_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `manager_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `role_id` INT NOT NULL,
+  `manager_id` INT NOT NULL,
   `reporting_manager` VARCHAR(30) NOT NULL
-  );
+    );
 
   SHOW TABLES;
--- Develop/db/seed.sql
+++ Develop/db/seed.sql
@@ -5,12 +5,12 @@ VALUES (1, "SALES", 12),
         (4, "ENGINEERING", 18),
         (5, "EXECUTIVE", 1);
 
-INSERT INTO role(role_id, job_title, salary, department_id) 
+INSERT INTO roles(role_id, job_title, salary, department_id) 
 VALUES (10, "OUTSIDE SALES", 75000, 12),
         (18, "TAX ATTORNEY", 185000, 8),
         (17, "SENIOR FINANCIAL ANALYST", 23),
         (9, "STRUCTURAL ENGINEER", 11800, 18),
-        (1, "CHAIRMAN AND CEO", 540000, 1)
+        (1, "CHAIRMAN AND CEO", 540000, 1);
 
 
 
